b3b178feb4910987b311532f63a71b745c90a534
Cosmin Valentin IOAN <cosmin-valentin.ioan@thalesgroup.com>
2019-03-08 11:33:35 +0200

    BLTOUCH disabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,8 +723,8 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-//#define BLTOUCH
-//#define SERVO0_PIN 27
+// #define BLTOUCH
+// #define SERVO0_PIN 27
 #if ENABLED(BLTOUCH)
   //#define BLTOUCH_DELAY 375   // (ms) Enable and increase if needed
 #endif
